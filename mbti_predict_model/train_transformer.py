3f20d3ce301675db96e9a9f7b0e0e60577d716a7
BuildTools <unconfigured@null.spigotmc.org>
2024-05-02 21:20:34 -0500

    transformer done

--- mbti_predict_model/train_transformer.py
+++ mbti_predict_model/train_transformer.py
@@ -9,29 +9,51 @@ from torch.utils.data import DataLoader
 checkpoint = 'bert-base-cased'
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
+batch_size = 4
+
 df = pd.read_csv("./Data_Process/mbti_1.csv")
+
+token_mbti = {"[PAD]" : 0, "[CLS]" : 1, "[SOS]" : 2, "I" : 3, "E" : 4, "N" : 5, "S" : 6, "F" : 7, "T" : 8, "J" : 9, "P" : 10}
+
+for i in range(len(df["type"])):
+    df["type"][i] = [1, token_mbti[df["type"][i][0]], token_mbti[df["type"][i][1]], token_mbti[df["type"][i][2]], token_mbti[df["type"][i][3]], 2]
+
+
 data = [(df["posts"][i], df["type"][i]) for i in range(len(df["posts"]))]
-dataset = Transformer_Dataset(data, tokenizer)
+
+print(df["type"][0])
+
+dataset = Transformer_Dataset(data, tokenizer, seq_len=128)
+
+
 
 train_loader = DataLoader(
-    dataset, batch_size=1, shuffle=True, pin_memory=True)
+    dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
+
+
 
 
 src_vocab_size = tokenizer.vocab_size
-tgt_vocab_size = tokenizer.vocab_size
-d_model = 512
+tgt_vocab_size = 11 # [PAD], [SOS], [CLS], I, E, N, S, F, T, J, P
+d_model = 128
 num_heads = 8
 num_layers = 6
-d_ff = 2048
-max_seq_length = 512
+d_ff = 512
+max_seq_length = 128
 dropout = 0.1
 
-transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout, device = "cpu")
+transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout, device = "cuda")
 
-trainer = Transformer_Trainer(transformer, train_loader, device='cpu')
+transformer.cuda()
 
 prev_epochs = 0
-epochs = 20
+epochs = 3
+
+
+# num_training_steps = epochs * (num_data_sample/batch_size)
+trainer = Transformer_Trainer(transformer, train_loader, num_warmup_steps= 1000, num_training_steps = epochs * (len(dataset)/batch_size),
+                              padding_token=0, device='cuda')
+
 
 
 for epoch in range(prev_epochs, epochs):
--- mbti_predict_model/transformer.py
+++ mbti_predict_model/transformer.py
@@ -5,7 +5,8 @@ import math
 import re
 from torch.utils.data import DataLoader, Dataset
 import tqdm
-
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
 
 
 class FeedForward(nn.Module):
@@ -43,7 +44,7 @@ class EncoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
     
     def forward(self, x, mask):
-        attn_output = self.self_attn(x, x, x, mask)
+        attn_output = self.self_attn(x, x, x, key_padding_mask = mask)[0]        
         x = self.norm1(x + self.dropout(attn_output))
         ff_output = self.feed_forward(x)
         x = self.norm2(x + self.dropout(ff_output))
@@ -52,17 +53,19 @@ class EncoderLayer(nn.Module):
 class DecoderLayer(nn.Module):
     def __init__(self, d_model, num_heads, d_ff, dropout):
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, num_heads)
-        self.cross_attn = nn.MultiheadAttention(d_model, num_heads)
+        self.self_attn = nn.MultiheadAttention(d_model, num_heads, batch_first=True)
+        self.cross_attn = nn.MultiheadAttention(d_model, num_heads, batch_first=True)
         self.feed_forward = FeedForward(d_model, d_ff)
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
         self.norm3 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
+        self.num_heads = num_heads
     
-    def forward(self, x, enc_output, src_mask, target_mask):
-        x = self.norm1(x + self.dropout(self.self_attn(x, x, x, target_mask)))
-        x = self.norm2(x + self.dropout(self.cross_attn(x, enc_output, enc_output, src_mask)))
+    def forward(self, x, enc_output, src_mask, tgt_padding_mask, tgt_future_mask):
+        attn_mask = tgt_future_mask.repeat(self.num_heads, 1, 1)
+        x = self.norm1(x + self.dropout(self.self_attn(x, x, x, key_padding_mask = tgt_padding_mask, attn_mask = attn_mask)[0]))
+        x = self.norm2(x + self.dropout(self.cross_attn(x, enc_output, enc_output, src_mask)[0]))
         x = self.norm3(x + self.dropout(self.feed_forward(x)))
         return x
     
@@ -72,41 +75,107 @@ class Transformer(nn.Module):
         self.encoder_embedding = nn.Embedding(src_vocab_size, d_model).to(device)
         self.decoder_embedding = nn.Embedding(tgt_vocab_size, d_model).to(device)
         self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+
+        self.tgt_vocab_size = tgt_vocab_size
         
         self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
         self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
 
         self.fc = nn.Linear(d_model, tgt_vocab_size).to(device)
         self.dropout = nn.Dropout(dropout).to(device)
+        self.softmax = nn.Softmax(dim = -1)
 
         self.device = device
 
     def generate_mask(self, src, tgt):
-        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
-        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
-
-        seq_length = tgt.size(1)
-        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool().to(self.device)
         
-        tgt_mask = tgt_mask & nopeak_mask
-        return src_mask, tgt_mask
+        src_mask = (src == 0)
+        tgt_padding_mask = (tgt == 0)
+
+        batch_size = tgt.size(0)
+        seq_length = tgt.size(1)
+        no_future_mask = (torch.triu(torch.ones(batch_size, seq_length, seq_length), diagonal=1)).bool().to(self.device)
+
+        return src_mask, tgt_padding_mask, no_future_mask
 
 
-    def forward(self, src, tgt):
-        src_mask, tgt_mask = self.generate_mask(src, tgt)
-        temp = self.encoder_embedding(src)
-        src_embedded = self.dropout(self.positional_encoding(temp))
-        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+    def decode(self, src, tgt):
+        res = []
+
+        src_mask, tgt_padding_mask, tgt_future_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
 
         enc_output = src_embedded
         for enc_layer in self.encoder_layers:
             enc_output = enc_layer(enc_output, src_mask)
 
-        dec_output = tgt_embedded
-        for dec_layer in self.decoder_layers:
-            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+        for i in range(4):
+            new_tgt = tgt.clone()
+            new_tgt[:, i+1:5] = 0
 
-        output = self.fc(dec_output)
+            tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(new_tgt)))
+            dec_output = tgt_embedded
+            for dec_layer in self.decoder_layers:
+                dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_padding_mask, tgt_future_mask)
+
+            output = self.softmax(self.fc(dec_output))
+            
+
+            res.append(output[:, i])
+        
+        return torch.stack(res, dim=1).view(-1, self.tgt_vocab_size)
+    
+
+    def prediction(self, src, tgt):
+        res = []
+
+        src_mask, tgt_padding_mask, tgt_future_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+
+        enc_output = src_embedded
+        for enc_layer in self.encoder_layers:
+            enc_output = enc_layer(enc_output, src_mask)
+
+        for i in range(4):
+            
+            tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+            dec_output = tgt_embedded
+            for dec_layer in self.decoder_layers:
+                dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_padding_mask, tgt_future_mask)
+            
+            output = self.softmax(self.fc(dec_output))
+            
+            cur_output = output[:, i]
+
+            res.append(cur_output)
+            
+
+            tgt[:, i] = cur_output.argmax(-1)
+        
+        return tgt[:, :4]
+        # return torch.stack(res, dim=1).view(-1, self.tgt_vocab_size)
+
+    def forward(self, src, tgt, tgt_len):
+        src_mask, tgt_padding_mask, tgt_future_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+
+        
+
+        enc_output = src_embedded
+
+        for enc_layer in self.encoder_layers:
+            enc_output = enc_layer(enc_output, src_mask)
+
+
+        dec_output = tgt_embedded
+
+        for dec_layer in self.decoder_layers:
+            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_padding_mask, tgt_future_mask)
+
+
+        output = self.softmax(self.fc(dec_output))[:, tgt_len]
+
         return output
 
 
@@ -117,6 +186,9 @@ class Transformer_Trainer:
         train_dataloader, 
         test_dataloader=None, 
         lr= 1e-4,
+        num_warmup_steps = 0,
+        num_training_steps = 0,
+        padding_token = 0,
         device='cuda'
         ):
 
@@ -125,8 +197,10 @@ class Transformer_Trainer:
         self.test_data = test_dataloader
         self.device = device
 
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(model.parameters(), lr = lr)
+        self.criterion = nn.CrossEntropyLoss(ignore_index=padding_token)
+        self.optimizer = optim.AdamW(model.parameters(), lr = lr)
+        self.scheduler = get_linear_schedule_with_warmup(self.optimizer, num_warmup_steps=num_warmup_steps, 
+    num_training_steps=num_training_steps)
 
     def train(self, epoch):
         self.iteration(epoch, self.train_data)
@@ -154,24 +228,29 @@ class Transformer_Trainer:
 
             # 0. batch_data will be sent into the device(GPU or cpu)
             data = {key: value.to(self.device) for key, value in data.items()}
-            
+
             # 1. forward the input data to get output
-            output = self.model.forward(data["Encoder_input"], data["Decoder_input"])
+            output = self.model.decode(data["Encoder_input"], data["Decoder_input"])
+            print(output.argmax(-1))
+            # output = self.model.forward(data["Encoder_input"], data["Decoder_input"])
             
-            # print(winner_output.shape)
             # 2-1. Crossentroyp loss of winner classification result
             # loss = self.criterion(winner_output, (data["winner_label"]))
-            loss = self.criterion(output, data["Decoder_output"])
+            loss = self.criterion(output, data["Decoder_output"].view(-1))
+
+            # print(output)
 
             # 3. backward and optimization only in train
             if train:
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+                self.scheduler.step()
+
 
             # next sentence prediction accuracy
             # correct = winner_output.argmax(dim=-1).eq(data["winner_label"]).sum().item()
-            correct = torch.round(output).eq(data["Decoder_output"]).sum().item()
+            correct = output.argmax(-1).eq(data["Decoder_output"].view(-1)).sum().item()
             avg_loss += loss.item()
             total_correct += correct
             total_element += data["Decoder_output"].nelement()
@@ -208,15 +287,17 @@ class Transformer_Dataset(Dataset):
         # Step 1: get random sentence pair, either negative or positive (saved as is_next_label)
         encoder_input, decoder_inoutput = self.data[item]
         
-        encoder_input = self.tokenizer(self.remove_urls(encoder_input), padding='max_length')['input_ids'][:512]
+        encoder_input = self.tokenizer(self.remove_urls(encoder_input), padding='max_length')['input_ids'][:self.seq_len]
         
-        tokened_res = self.tokenizer(" ".join(decoder_inoutput))['input_ids']
-        decoder_input = tokened_res[:-1] + [0] * 507
-        decoder_output = tokened_res[1:] + [0] * 507
+
+        # tokened_res = self.tokenizer(" ".join(decoder_inoutput))['input_ids']
+        decoder_input = decoder_inoutput[:-1] + [0] * (self.seq_len - 5)
+        # decoder_output = tokened_res[1:] + [0] * (self.seq_len - 5)
+        decoder_output = decoder_inoutput[1:-1]
 
         output = {"Encoder_input" : encoder_input, "Decoder_input" : decoder_input, "Decoder_output" : decoder_output}
 
-
+        
         return {key: torch.tensor(value) for key, value in output.items()}
     
 
